9a8cfd19c006a2d153b5b223bf3713ed5f205135
Kantaro Watanabe <kankankan.w0528@gmail.com>
2020-04-21 20:54:27 +0900

    add file

---
+++ app/helpers/final_static_pages_helper.rb
@@ -0,0 +1,2 @@
+module FinalStaticPagesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'final_static_pages/home'
+  get 'final_static_pages/help'
+  get 'static_pages/home'
+  get 'static_pages/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
